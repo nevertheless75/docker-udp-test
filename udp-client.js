f0d42f4db5ee8593f5d9ed10e351ef8a80055706
zakmckracken1988 <andreas@thing-it.com>
2017-07-28 11:23:54 +0200

    Command line parameter support

--- udp-client.js
+++ udp-client.js
@@ -1,8 +1,22 @@
-var PORT = 47809;
-var HOST = '127.0.0.1';
+const commandLineArgs = require('command-line-args');
+
+const optionDefinitions = [
+    { name: 'host', alias: 'h', type: String },
+    { name: 'port',  alias: 'p', type: Number }
+];
+
+const options = commandLineArgs(optionDefinitions);
 
 var dgram = require('dgram');
-var message = new Buffer('Wurstsalat!');
+
+var PORT = 47809;
+if (options.port)
+    PORT = options.port;
+var HOST =  '0.0.0.0';
+if (options.host)
+    HOST = options.host;
+
+var message = new Buffer('Wurstsalat! Send to ' + HOST + ":" + PORT);
 
 var client = dgram.createSocket('udp4');
 client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
--- udp-server.js
+++ udp-server.js
@@ -1,7 +1,21 @@
-var PORT = 47809;
-var HOST = '0.0.0.0';
+const commandLineArgs = require('command-line-args');
+
+const optionDefinitions = [
+    { name: 'host', alias: 'h', type: String },
+    { name: 'port',  alias: 'p', type: Number }
+];
+
+const options = commandLineArgs(optionDefinitions);
 
 var dgram = require('dgram');
+
+var PORT = 47809;
+if (options.port)
+    PORT = options.port;
+var HOST =  '0.0.0.0';
+if (options.host)
+    HOST = options.host;
+
 var server = dgram.createSocket('udp4');
 
 server.on('listening', function () {
